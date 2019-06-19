d6fa8a3f631001b9c4abc7ee1ab4f585b13899b5
sdim-ua <ingersol@yandex.ua>
2019-06-20 00:12:29 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+/* 
+Класс Human и интерфейсы CanRun, CanSwim
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add public interfaces and a public class here - добавь public интерфейсы и public класс тут
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+    
+     public abstract class Human implements CanRun, CanSwim{
+         
+     }
+    
+    
+}
